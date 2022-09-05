cf30b04b4884abf2255571d719c5006cd54dd126
tesfalemt <tesfalemtekie27@gmail.com>
2022-09-05 15:06:10 -0500

    WAA

--- Online Dictionary/dict.js
+++ Online Dictionary/dict.js
@@ -1,37 +1,41 @@
-$(document).ready(function () {
-  $("#submit").click(function () {
-    $.ajax({
-      method: "POST",
-      url: "http://localhost:9009/search",
-      data: JSON.stringify({
-        searchTerm: document.getElementById("searchKey").value,
-      }),
-      contentType: "application/json; charset=utf-8",
-      // dataType: "json",
-      success: showSearchResult,
-      error: function (err) {
-        console.log(err);
-      },
-    });
-  });
+// $(document).ready(function () {
+//   $("#submit").click(function () {
+//     $.ajax({
+//       method: "POST",
+//       url: "http://localhost:9009/search",
+//       data: JSON.stringify({
+//         searchTerm: document.getElementById("searchKey").value,
+//       }),
+//       contentType: "application/json; charset=utf-8",
+//       // dataType: "json",
+//       success: showSearchResult,
+//       error: function (err) {
+//         console.log(err);
+//       },
+//     });
+//   });
 
-  function removePerviousResult() {
-    $("#result").remove();
-  }
-  function showSearchResult(data) {
-    removePerviousResult();
+//   function removePerviousResult() {
+//     $("#result").remove();
+//   }
+//   function showSearchResult(data) {
+//     removePerviousResult();
 
-    if (data.length > 0) {
-      var ol = `<ol id="result"></ol>`;
-      $("#searchresult").append(ol);
+//     if (data.length > 0) {
+//       var ol = `<ol id="result"></ol>`;
+//       $("#searchresult").append(ol);
 
-      data.forEach(function (element) {
-        $("#result").append(
-          `<li>${element.word}(${element.wordtype})   :   ${element.definition}</li>`
-        );
-      });
-    } else {
-      alert(`No search result found!!`);
-    }
-  }
-});
+//       data.forEach(function (element) {
+//         $("#result").append(
+//           `<li>${element.word}(${element.wordtype})   :   ${element.definition}</li>`
+//         );
+//       });
+//     } else {
+//       alert(`No search result found!!`);
+//     }
+//   }
+// });
+
+const os = require('os');
+// const app = os();
+console.log(os.endianness());
